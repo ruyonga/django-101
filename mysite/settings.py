1542b46ab733f097c76778965a4cfeed60a1f471
Ruyonga Dan <druyonga@gmail.com>
2023-11-16 23:25:22 +0900

    add form and views

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         "NAME": "polls",
         "USER": "postgres",
-        "PASSWORD": "toor",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import  get_object_or_404, render
+from django.template import loader
+from django.http import Http404
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, Daiki, You're at the polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = { "latest_question_list" : latest_question_list}
 
+    return render(request, "polls/index.html", context)
+
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
 
 def detail(request, question_id):
-    response  = "You're looking at the results of questions %s"
-    return HttpResponse(response%question_id)
+    
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
